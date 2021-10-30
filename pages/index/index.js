def18dd0a3f4be33356f1cf71fcd839791476f66
messi1991 <603375508@qq.com>
2021-10-30 23:06:34 +0800

    分类接口对接

--- pages/index/index.js
+++ pages/index/index.js
@@ -16,56 +16,8 @@ Page({
     isShowCalendar: false,
     minDate: new Date(2010, 0, 1).getTime(),
     date: formatDate(''),
-    list: [
-      {
-        title: '餐饮',
-        icon: 'cart-circle-o'
-      },
-      {
-        title: '购物',
-        icon: 'cart-circle-o'
-      },
-      {
-        title: '超市',
-        icon: 'cart-circle-o'
-      },
-      {
-        title: '交通',
-        icon: 'cart-circle-o'
-      },
-      {
-        title: '医疗',
-        icon: 'cart-circle-o'
-      },
-      {
-        title: '学习',
-        icon: 'cart-circle-o'
-      },
-      {
-        title: '礼金',
-        icon: 'cart-circle-o'
-      },
-      {
-        title: '日用',
-        icon: 'cart-circle-o'
-      },
-      {
-        title: '通讯',
-        icon: 'cart-circle-o'
-      },
-      {
-        title: '美容',
-        icon: 'cart-circle-o'
-      },
-      {
-        title: '健身',
-        icon: 'cart-circle-o'
-      },
-      {
-        title: '其他',
-        icon: 'cart-circle-o'
-      },
-    ],
+    list: [],
+    listIndex: 0,
     value: ''
   },
   onCalendarShow() {
@@ -84,10 +36,11 @@ Page({
   onChange(event) {
     
   },
-  onClick(index) {
+  onClick({ currentTarget: { dataset: { index = 0 } } }) {
     console.log("index", index);
     this.setData({
-      show: true
+      show: true,
+      listIndex: index
     })
   },
   onClose() {
@@ -99,11 +52,13 @@ Page({
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: async function (options) {
 
     // { method: 'POST' }
-    prequest('/users').then(res => {
-      console.log('查看值', res)
+    const { data } = await prequest('/types');
+    console.log("data", data);
+    this.setData({
+      list: data
     })
   },
 
